696bbf98e16d532ccb56e657fbf6361706ec51c7
ayenter <alecyenter@csu.fullerton.edu>
2015-04-15 22:29:54 -0700

    final draft - complete ...hopefully

--- assignment8.js
+++ assignment8.js
@@ -39,39 +39,68 @@ app.on("error", function (err) {
     console.log("error - " + err);
 });
 
-function ValidURL(str) {
+function fixAddress(addr) {
     "use strict"
-    return (str.substring(0, 11) !== "http://loca");
+    if (addr.length != 4) {
+        if (addr.length >= 7 && addr.substring(0, 7) !== "http://") {
+            addr = "http://" + addr;
+        }
+        if (addr.length >= 22 && addr.substring(0, 22) === "http://localhost:3000/") {
+            addr = addr.substring(22);
+        } else if (addr.length >= 11 && addr.substring(0, 11) !== "http://www.") {
+            addr = "http://www." + addr.substring(7);
+        }
+    } else if (addr.length < 4) {
+        addr = null;
+    }
+    return addr;
 }
 
 app.get('/', function(req, res) {
     //var popular = Link.find({}).sort('hits').limit(10);
     "use strict";
-    res.render('index', {pop: "", output: ""});
+    Link.find({}).select("short long").sort({hits: -1}).limit(10).exec(function(err,topten) {
+        res.render('index2', {top: JSON.stringify(topten), output: ""});
+    });
 });
 
 app.post('/', function(req, res) {
     "use strict";
-    var url, test, output_url;
     var input = req.body.input;
-    INIT_KEY += 1;
-    var key = (INIT_KEY).toString(36);
-    var newlink = new Link({ long: input, short: key, hits: 0});
-    newlink.save();
-    res.render('index', {top: "", output: "http://localhost:3000/" + key});
+    input = fixAddress(input);
+    var output;
+    if (input != null){
+        Link.find({long: input}, function(err, longlist) {
+            Link.find({short: input}, function(err, shortlist) {
+                Link.find({}).sort({hits: -1}).limit(10).exec(function(err,topten) {
+                    if (longlist.length!=0) {
+                        output = "http://localhost:3000/" + longlist[0].short;
+                    } else if (shortlist.length!=0) {
+                        output = shortlist[0].long;
+                    } else {
+                        INIT_KEY += 1;
+                        var key = (INIT_KEY).toString(36);
+                        var newlink = new Link({ long: input, short: key, hits: 0});
+                        newlink.save();
+                        output = "http://localhost:3000/" + key;
+                    }
+                    res.render('index2', {top: JSON.stringify(topten), output: output});
+                });
+            });
+        });
+    }
 });
 
-
+//http://mongoosejs.com/docs/2.7.x/docs/updating-documents.html
 app.all('/:key', function(req, res) {
-    Link.find({"short" : req.params.key}, function(err, reply) {
-    //db.get("short:"+req.params.key, function(err, reply) {
-        "use strict";
-        console.log(reply);
-        if(reply!=null && err==null){
-            res.redirect(reply[0].long);
-
+    "use strict";
+    Link.findOne({"short" : req.params.key}, function(err, reply) {
+        if(reply!=null && reply.length!=0 && err==null){
+            reply.hits = reply.hits + 1;
+            reply.save();
+            res.redirect(reply.long);
         } else {
-            res.render("index", {output: "NOT FOUND"});
+            res.render("index2", {top: JSON.stringify(["NOT FOUND"]), output: ""});
         }
     });
     
